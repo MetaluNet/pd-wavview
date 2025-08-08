a11e5bdfc6c2bc9a2f28faf0fcf4eadf0dd74af5
Antoine Rousseau <_antoine_@metalu.net>
2025-08-08 19:25:05 +0200

    add upload script

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJECT=wavview
+VERSION=$(git describe --abbrev=0)
+EXPORTDIR=$PROJECT
+rm -rf $EXPORTDIR
+mkdir -p $EXPORTDIR
+cp *.pd *.wav *.md LICENSE $EXPORTDIR
+deken upload -v $VERSION $PROJECT
+
+
